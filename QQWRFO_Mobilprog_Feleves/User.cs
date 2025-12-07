44c888d8ce38b5a61991b66d066598210e07ad57
Fulion001 <114493042+Fulion001@users.noreply.github.com>
2025-12-07 23:46:45 +0100

    user class updated, registerPage changed according to class

--- QQWRFO_Mobilprog_Feleves/User.cs
+++ QQWRFO_Mobilprog_Feleves/User.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
 
 namespace QQWRFO_Mobilprog_Feleves
 {
     public class User
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Username { get; set; }
+
+        public string HashedPassword { get; set; }
+
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
     }
 }

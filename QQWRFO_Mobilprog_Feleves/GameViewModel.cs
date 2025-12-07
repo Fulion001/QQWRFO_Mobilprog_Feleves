e7e2ef572cd5c24866e4ebcf64fd68654085ff7a
Fulion001 <114493042+Fulion001@users.noreply.github.com>
2025-12-07 23:29:06 +0100

    PuzzlePiece class added

--- QQWRFO_Mobilprog_Feleves/GameViewModel.cs
+++ QQWRFO_Mobilprog_Feleves/GameViewModel.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace QQWRFO_Mobilprog_Feleves
 {
-    internal class GameViewModel : 
+    public class PuzzlePiece : ObservableObject
+    {
+        private ImageSource _imageSource;
+
+        public ImageSource ImageSource
+        {
+            get => _imageSource;
+            set => SetProperty(ref _imageSource, value);
+        }
+
+        public int OriginalIndex { get; set; } // Helyes pozíció 0-tól N-ig
+        public int CurrentIndex { get; set; }  // Aktuális pozíció
+    }
+    internal class GameViewModel
     {
     }
 }

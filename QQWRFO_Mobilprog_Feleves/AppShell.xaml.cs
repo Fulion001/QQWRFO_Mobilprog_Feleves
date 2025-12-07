bc2079623475200bb9032048018aee2144e3a7b7
Fulion001 <114493042+Fulion001@users.noreply.github.com>
2025-12-07 20:38:47 +0100

    Add project files.

---
+++ QQWRFO_Mobilprog_Feleves/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QQWRFO_Mobilprog_Feleves
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

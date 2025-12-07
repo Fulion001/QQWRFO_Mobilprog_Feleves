dca10b6532e619b61aa19db4fc01027b9bde41ed
Fulion001 <114493042+Fulion001@users.noreply.github.com>
2025-12-07 23:51:06 +0100

    mauiProgram.cs database injection

--- QQWRFO_Mobilprog_Feleves/MauiProgram.cs
+++ QQWRFO_Mobilprog_Feleves/MauiProgram.cs
@@ -14,9 +14,9 @@ namespace QQWRFO_Mobilprog_Feleves
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
+            builder.Services.AddSingleton<IDatabaseService, SqliteDatabaseService>();
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
